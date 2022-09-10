f24803e5902cae3b9dcc301123931b39f7cd2ca3
stas <stasparsuk@gmail.com>
2022-09-10 10:34:52 +0300

    setting

--- public/setting.php
+++ public/setting.php
@@ -1,1 +1,5 @@
 <?php
+ini_set('display_errors', 1);//вывод ошибок
+ini_set('display_startup_errors', 1);//вывод ошибок
+error_reporting(E_ALL);//вывод ошибок
+require_once "../vendor/autoload.php";
